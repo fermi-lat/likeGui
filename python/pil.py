4c67cf72777edb7c81c88f38ae5833ddaee3a24c
jchiang <>
2004-05-29 00:14:51 +0000

    add pil interface

---
+++ python/pil.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+Interface to .par files.
+
+@author J. Chiang
+"""
+#
+#$Header: /nfs/slac/g/glast/ground/cvs/users/jchiang/likeGui/python/pil.py,v 1.1 2004/04/14 16:51:56 jchiang Exp $
+#
+
+def accept(line):
+    if (line.find('#') == 0 or len(line.split()) == 0):
+        return 0
+    return 1
+
+def name(line):
+    return line.split(',')[0].strip()
+
+def fields(line):
+    return [item.strip() for item in line.split(',')[1:]]
+
+class Pil(object):
+    def __init__(self, parfile):
+        self.params = {}
+        self.names = []
+        file = open(parfile, 'r')
+        for line in file:
+            if accept(line):
+                self.params[name(line)] = fields(line)
+                self.names.append(name(line))
+    def keys(self):
+        return self.names
+    def __getitem__(self, name):
+        return (self.params[name][2]).strip('"').strip("'")
+    def __setitem__(self, name, value):
+        self.params[name][2] = `value`
+    def __call__(self):
+        args = ''
+        for name in self.keys():
+            args += ' ' + self.__getitem__(name)
+        return args
+
+if __name__ == '__main__':
+    pars = Pil('likelihood.par')
+    print pars['event_file']
+    print pars['Spacecraft_file_hdu']
+    pars['Spacecraft_file_hdu'] = 3
+    pars['event_file'] = 'foo'
+    print pars['Spacecraft_file_hdu']
+    print pars()
+
