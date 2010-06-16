1c3f95fbd2de5275fe9bad92b5b2567518ca0707
jchiang <>
2010-06-16 22:44:39 +0000

    add includes for gcc4.4.3 build

--- src/ModelEditor.cxx
+++ src/ModelEditor.cxx
@@ -4,9 +4,10 @@
  *
  * @author J. Chiang <jchiang@slac.stanford.edu>
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/likeGui/src/ModelEditor.cxx,v 1.3 2006/02/07 22:45:56 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/likeGui/src/ModelEditor.cxx,v 1.4 2006/02/08 02:24:50 jchiang Exp $
  */
 
+#include <cstdlib>
 #include <sstream>
 
 int main(int iargc, char * argv[]) {
