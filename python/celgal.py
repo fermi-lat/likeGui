2ce1bcb2c3db62d5925046a23a02d50dd2c359c5
jchiang <>
2015-02-04 16:38:35 +0000

    use astropy.coordinates in celgal.py

--- python/celgal.py
+++ python/celgal.py
@@ -4,102 +4,48 @@ Class for transforming between Equatorial and Galactic coordinates.
 
 @author J. Chiang <jchiang@slac.stanford.edu>
 
-$Header: /nfs/slac/g/glast/ground/cvs/likeGui/python/celgal.py,v 1.7 2005/04/13 22:22:22 jchiang Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/likeGui/python/celgal.py,v 1.8 2005/04/19 13:39:56 jchiang Exp $
 """
 
-try:
-    from numarray import *
-except ImportError:
-    import math
-    from math import *
-    arcsin = asin
-    arccos = acos
-    arctan2 = atan2
+from numpy import *
+import astropy.coordinates as coords
+import astropy.units as units
 
-class celgal:
-    def __init__(self, J2000=1):
-        #
-        # Rotation angles for the two most common epochs
-        #
+class celgal(object):
+    def __init__(self, J2000=True):
         if J2000:
-            self.zrot1 = 282.8592
-            self.xrot = 62.8717
-            self.zrot2 = 32.93224
-        else: # use B1950 values
-            self.zrot1 = 282.25
-            self.xrot = 62.6
-            self.zrot2 = 33.
-
-        self.cos_xrot = cos(self.xrot*pi/180.)
-        self.sin_xrot = sin(self.xrot*pi/180.)
-
+            self.celestialCoords = coords.FK5     # J2000
+        else:
+            self.celestialCoords = coords.FK4     # B1950
     def gal(self, radec):
-        (ra, dec) = radec
-        return (self.glon(ra, dec), self.glat(ra, dec))
-
+        cel_coord = coords.SkyCoord(radec[0], radec[1],
+                                    self.celestialCoords,
+                                    unit=(units.degree, units.degree))
+        gal_coord = cel_coord.transform_to(coords.Galactic)
+        return gal_coord.l.degree, gal_coord.b.degree
     def cel(self, lb):
-        (glon, glat) = lb
-        return (self.RA(glon, glat), self.DEC(glon, glat))
+        gal_coord = coords.SkyCoord(lb[0], lb[1],
+                                    coords.Galactic,
+                                    unit=(units.degree, units.degree))
+        cel_coord = gal_coord.transform_to(self.celestialCoords)
+        return cel_coord.ra.degree, cel_coord.dec.degree
+    def glon(self, ra, dec):
+        l, b = self.gal((ra, dec))
+        return l
+    def glat(self, ra, dec):
+        l, b = self.gal((ra, dec))
+        return b
+    def RA(self, l, b):
+        lon, lat = self.cel((l, b))
+        return lon
+    def DEC(self, l, b):
+        lon, lat = self.cel((l, b))
+        return lat
 
-    def glon(self, ra, dec) :
-        """Galactic longitude as a function of Equatorial coordinates"""
-        sdec = sin(dec*pi/180.)
-        cdec = cos(dec*pi/180.)
-        sdra = sin((ra-self.zrot1)*pi/180.)
-        cdra = cos((ra-self.zrot1)*pi/180.)
-        glon = self.zrot2 + arctan2(cdec*sdra*self.cos_xrot+sdec*self.sin_xrot,
-                                    cdec*cdra)*180./pi
-        try:
-            if glon < 0. : glon += 360.
-            if glon > 360. : glon -= 360.
-        except RuntimeError:
-            for i in xrange(len(glon)):
-                if glon[i] < 0.: glon[i] += 360.
-                if glon[i] > 360.: glon[i] -= 360.
-        return glon
-
-    def glat(self, ra, dec) :
-        """Galactic latitude as a function of Equatorial coordinates"""
-        sdec = sin(dec*pi/180.)
-        cdec = cos(dec*pi/180.)
-        sdra = sin((ra-self.zrot1)*pi/180.)
-        return arcsin(sdec*self.cos_xrot-cdec*sdra*self.sin_xrot)*180./pi
-            
-    def RA(self, longitude, latitude) :
-        """Right ascension as a function of Galactic coordinates"""
-        clat = cos(latitude*pi/180.)
-        slat = sin(latitude*pi/180.)
-        cdlon = cos((longitude-self.zrot2)*pi/180.)
-        sdlon = sin((longitude-self.zrot2)*pi/180.)
-        ra = self.zrot1 + arctan2(clat*sdlon*self.cos_xrot-slat*self.sin_xrot,
-                                  clat*cdlon)*180./pi
-        try:
-            if ra < 0. : ra = ra + 360.
-            if ra > 360. : ra = ra - 360.
-        except RuntimeError:
-            for i in xrange(len(ra)):
-                if ra[i] < 0.: ra[i] += 360.
-                if ra[i] > 360.: ra[i] -= 360.
-        return ra
-
-    def DEC(self, longitude, latitude) :
-        """Declination as a function of Galactic coordinates"""
-        clat = cos(latitude*pi/180.)
-        slat = sin(latitude*pi/180.)
-        sdlon = sin((longitude-self.zrot2)*pi/180.)
-        return arcsin(clat*sdlon*self.sin_xrot+slat*self.cos_xrot)*180./pi
-    
 def dist(a, b):
-    """Angular separation in degrees between two sky coordinates"""
-    ra1, dec1 = a
-    ra2, dec2 = b
-    ra1 = ra1*pi/180.
-    dec1 = dec1*pi/180.
-    ra2 = ra2*pi/180.
-    dec2 = dec2*pi/180.
-    mu = (cos(dec1)*cos(ra1)*cos(dec2)*cos(ra2)
-          + cos(dec1)*sin(ra1)*cos(dec2)*sin(ra2) + sin(dec1)*sin(dec2))
-    return Angdist(mu)*180./pi
+    a_coord = coords.SkyCoord(a[0], a[1], unit=(units.degree, units.degree))
+    b_coord = coords.SkyCoord(b[0], b[1], unit=(units.degree, units.degree))
+    return a_coord.separation(b_coord).degree
 
 def SphCoords(u):
     import math
@@ -131,12 +77,12 @@ if __name__ == "__main__":
     #
     converter = celgal()
     sources = {}
-    sources["Crab"] = ( (83.5665, 22.0078), (184.53, -5.84) )
-    sources["Geminga"] = ( (98.4909, 17.8605), (195.06, 4.32) )
-    sources["Vela"] = ( (128.7346, -45.2023), (263.53, -2.86) )
-    sources["3C_279"] = ( (193.9797, -5.8239), (304.98, 57.03) )
-    sources["GalCenter"] = ( (266.4051, -28.9362), (0, 0) )
-    tol = 1e-3
+    sources["Crab"] = ( (83.5665, 22.0078), (184.52996368, -5.83998396) )
+    sources["Geminga"] = ( (98.4909, 17.8605), (195.06003431, 4.31998806) )
+    sources["Vela"] = ( (128.7346, -45.2023), (263.52989939, -2.86004251) )
+    sources["3C_279"] = ( (193.9797, -5.8239), (304.97995250, 57.02996622) )
+    sources["GalCenter"] = ( (266.40508920, -28.93617470), (0, 0) )
+    tol = 2e-4
     for key in sources.keys():
         #print sources[key][0], converter.cel(sources[key][1])
         assert dist(sources[key][0], converter.cel(sources[key][1])) < tol
@@ -146,12 +92,12 @@ if __name__ == "__main__":
     # check epoch B1950 conversions
     #
     converter = celgal(0)
-    sources["Crab"] = ( (82.8143, 21.9748), (184.53, -5.84) )
-    sources["Geminga"] = ( (97.7614, 17.8998), (195.06, 4.32) )
-    sources["Vela"] = ( (128.3128, -45.0290), (263.53, -2.86) )
-    sources["3C_279"] = ( (193.3324, -5.5534), (304.98, 57.03) )
-    sources["GalCenter"] = ( (265.6108, -28.9168), (0, 0) )
-    tol = 1e-4
+    sources["Crab"] = ( (82.8143, 21.9748), (184.52995465, -5.83998847) )
+    sources["Geminga"] = ( (97.7614, 17.8998), (195.06003244, 4.31995548) )
+    sources["Vela"] = ( (128.3128, -45.0290), (263.53005171, -2.85999746) )
+    sources["3C_279"] = ( (193.3324, -5.5534), (304.97991590, 57.02999541) )
+    sources["GalCenter"] = ( (265.61084403, -28.91679035), (0, 0) )
+    tol = 2e-4
     for key in sources.keys():
         #print sources[key][0], converter.cel(sources[key][1])
         assert dist(sources[key][0], converter.cel(sources[key][1])) < tol
--- python/celgaltest.py
+++ python/celgaltest.py
@@ -6,7 +6,7 @@ web page: <http://nedwww.ipac.caltech.edu/>
 
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
-#$Header: /nfs/slac/g/glast/ground/cvs/likeGui/python/celgaltest.py,v 1.1.1.1 2004/04/29 17:30:48 jchiang Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/likeGui/python/celgaltest.py,v 1.2 2005/01/15 06:11:32 jchiang Exp $
 
 import unittest
 import celgal as celgal
@@ -15,24 +15,24 @@ class CelgalTestCases(unittest.TestCase):
     def testJ2000Conversions(self):
         converter = celgal.celgal(1)
         sources = {}
-        sources["Crab"] = ( (83.5665, 22.0078), (184.53, -5.84) )
-        sources["Geminga"] = ( (98.4909, 17.8605), (195.06, 4.32) )
-        sources["Vela"] = ( (128.7346, -45.2023), (263.53, -2.86) )
-        sources["3C_279"] = ( (193.9797, -5.8239), (304.98, 57.03) )
-        sources["GalCenter"] = ( (266.4051, -28.9362), (0, 0) )
+        sources["Crab"] = ( (83.5665, 22.0078), (184.52996368, -5.83998396) )
+        sources["Geminga"] = ( (98.4909, 17.8605), (195.06003431, 4.31998806) )
+        sources["Vela"] = ( (128.7346, -45.2023), (263.52989939, -2.86004251) )
+        sources["3C_279"] = ( (193.9797, -5.8239), (304.97995250, 57.02996622) )
+        sources["GalCenter"] = ( (266.40508920, -28.93617470), (0, 0) )
         dist = celgal.dist
-        tol = 6e-4
+        tol = 2e-4
         for key in sources.keys():
             assert dist(sources[key][0], converter.cel(sources[key][1])) < tol
             assert dist(converter.gal(sources[key][0]), sources[key][1]) <tol
     def testB1950Conversions(self):
         converter = celgal.celgal(0)
         sources = {}
-        sources["Crab"] = ( (82.8143, 21.9748), (184.53, -5.84) )
-        sources["Geminga"] = ( (97.7614, 17.8998), (195.06, 4.32) )
-        sources["Vela"] = ( (128.3128, -45.0290), (263.53, -2.86) )
-        sources["3C_279"] = ( (193.3324, -5.5534), (304.98, 57.03) )
-        sources["GalCenter"] = ( (265.6108, -28.9168), (0, 0) )
+        sources["Crab"] = ( (82.8143, 21.9748), (184.52995465, -5.83998847) )
+        sources["Geminga"] = ( (97.7614, 17.8998), (195.06003244, 4.31995548) )
+        sources["Vela"] = ( (128.3128, -45.0290), (263.53005171, -2.85999746) )
+        sources["3C_279"] = ( (193.3324, -5.5534), (304.97991590, 57.02999541) )
+        sources["GalCenter"] = ( (265.61084403, -28.91679035), (0, 0) )
         dist = celgal.dist
         tol = 1e-4
         for key in sources.keys():
